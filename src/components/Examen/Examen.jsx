44daa18359f581a601ce94d38d4afb37042439d4
Francis <karma.police190@gmail.com>
2024-07-26 02:00:46 -0500

    Agregado diseño de home // comentarios las cosas que faltan

--- src/components/Examen/Examen.jsx
+++ src/components/Examen/Examen.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
-import RecordView from '../../Hooks/ReactMediaRecorder';
+import RecordView from '../../Hooks/RecordVideo.jsx';
 
 
 
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,13 +1,29 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
+import './Home.css'
 
 function Home() {
   return (
-    <div>
-      <h1>¡Bienvenido a la página principal!</h1>
+    <div className='home-cmp'>
+      {/* poner enlace de login */}
+      <nav className='nav-home-cmp'>
+        {/* falta poner el logo */}
+        <h2>LOGO EDUCA</h2>
+        <Link to="/examenes">
+          <button className='ingresar-boton-cmp'>INGRESAR</button>
+        </Link>
+      </nav>
+      <div className='frases-home-cmp'>
+        <h1>La educación es la clave para el desarrollo personal y social.</h1>
+        <p>Fomenta el pensamiento crítico, la creatividad y prepara a los individuos para enfrentar los desafios del futuro.</p>
+      </div>
+      {/* poner enlace de registro */}
+      <div className='registrar-cmp'>
       <Link to="/examenes">
-        <button>Ir a Examenes</button>
+
+        <button className='registrar-boton-cmp'>COMIENZA AHORA!</button>
       </Link>
+      </div>
     </div>
   );
 };
